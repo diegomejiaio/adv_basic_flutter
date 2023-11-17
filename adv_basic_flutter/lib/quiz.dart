fcda9c69bd7bd8f3aa0fc195025bccad5907fd06
Diego <66480977+diegomm796@users.noreply.github.com>
2023-11-16 22:06:17 -0500

    [feature] Save selected answer

--- adv_basic_flutter/lib/quiz.dart
+++ adv_basic_flutter/lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basic_flutter/start_screen.dart';
 import 'package:adv_basic_flutter/questions_screen.dart';
+import 'package:adv_basic_flutter/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -10,7 +11,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selecterAnswer = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -19,11 +20,25 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    print(selectedAnswers);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
   @override
   Widget build(context) {
     final screenWidget = activeScreen == 'start-screen'
         ? StartScreen(switchScreen)
-        : const QuestionsScreen();
+        : QuestionsScreen(
+            onSelectAnswer: chooseAnswer,
+          );
 
     return MaterialApp(
       home: Scaffold(
